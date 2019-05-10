16f5ec0ccaf36b362a205f2ec2648855817a89d3
P. Todd Decker <ptdecker@mac.com>
2019-05-09 21:19:17 -0500

    Moved hex2base64 into a function

--- cryptopal.go
+++ cryptopal.go
@@ -11,14 +11,8 @@ import (
 	"os"
 )
 
-func main() {
-
-	// Read in a line of text in from console
-	reader := bufio.NewReader(os.Stdin)
-	line, err := reader.ReadBytes('\n')
-	if err != nil && err != io.EOF {
-		log.Fatal(err)
-	}
+// Convert a slice of bytes containing hex encoded text into a base64 encoded string
+func asciiHex2Base64(line []byte) string {
 
 	// Decode hex encoded text into a byte slice trimming any trailing delimiter
 	src := bytes.TrimRight(line, string([]byte{'\n'}))
@@ -29,6 +23,18 @@ func main() {
 	}
 
 	// Output array of bytes in base64 encoding to console
-	fmt.Println(base64.StdEncoding.EncodeToString(dst[:n]))
+	 return base64.StdEncoding.EncodeToString(dst[:n])
 
+}
+
+func main() {
+
+	// Read in a line of text in from console
+	reader := bufio.NewReader(os.Stdin)
+	line, err := reader.ReadBytes('\n')
+	if err != nil && err != io.EOF {
+		log.Fatal(err)
+	}
+
+	fmt.Println(asciiHex2Base64(line))
 }
